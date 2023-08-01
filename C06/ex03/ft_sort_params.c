e1539c3a7af506d89bfaee112f4bcfe64b34d5c5
Hüseyin Ateş <109745789+Neptuun99@users.noreply.github.com>
2023-08-01 19:14:51 +0300

    Add files via upload

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: huates <huates@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/26 17:50:27 by huates            #+#    #+#             */
+/*   Updated: 2023/07/29 15:06:29 by huates           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2 && *s1 && *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
+
+int	main(int argc, char *argv[])
+{
+	char	*tmp;
+	int		i;
+	int		j;
+
+	i = 1;
+	while (argc > i)
+	{
+		j = i + 1;
+		while (argc > j)
+		{
+			if (ft_strcmp(argv[i], argv[j]) > 0)
+			{
+				tmp = argv[i];
+				argv[i] = argv[j];
+				argv[j] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+	i = 0;
+	while (argc > ++i)
+		ft_putchar(argv[i]);
+	return (0);
+}
